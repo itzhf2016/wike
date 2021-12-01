19e927852a367d5e4df54752778c45916851e024
it_zhf <it_zhf@163.com>
2021-12-01 22:05:26 +0800

    init

---
+++ src/main/java/com/kanghui/wike/WikeApplication.java
@@ -0,0 +1,13 @@
+package com.kanghui.wike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kanghui/wike/WikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanghui.wike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
